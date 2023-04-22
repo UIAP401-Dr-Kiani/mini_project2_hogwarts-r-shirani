3d609f18acb76287bc343b65f0ee6a9ade9365f6
r-shirani <rsh96785@gmail.com>
2023-04-22 16:17:13 +0430

    person property name was changed

--- hagwarts/hagwarts/Person.cs
+++ hagwarts/hagwarts/Person.cs
@@ -8,13 +8,13 @@ namespace hagwarts
 {
     public class Person
     {
-        public string PersonFirstName { get; set; }
-        public string PersonLastName { get; set; }
-        public string PersonBirthDay { get; set; }
-        public string PersonGender { get; set; }
-        public string PersonFatherName { get; set; }
-        public string PersonUserName { get; set; }
-        public string PersonPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDay { get; set; }
+        public string Gender { get; set; }
+        public string FatherName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
         public BreedType BreedType { get; set; }
 
     }
