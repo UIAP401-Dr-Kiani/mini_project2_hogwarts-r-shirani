7cc39519ab7f68bf9df75829954144c41c2067ea
r-shirani <rsh96785@gmail.com>
2023-04-22 14:12:31 +0430

    group class was created

---
+++ hagwarts/hagwarts/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hagwarts
+{
+    public class Group
+    {
+        public GroupType GroupType { get; set; }
+        public int score { get; set; }
+        public List<AllowedPerson> GroupMembers { get; set; }
+        public List<AllowedPerson> QuidditchPlayers { get; set; }
+    }
+}
