eb59f8a2ed551206b78b1aea7949c5bbe2563b4d
r-shirani <rsh96785@gmail.com>
2023-04-22 13:38:15 +0430

    breed type enum

---
+++ hagwarts/hagwarts/BreedType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hagwarts
+{
+    public enum BreedType
+    {
+        HalfBlood,
+        PureBlood,
+        MuggleBlood,
+    }
+}
