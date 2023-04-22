9ea2034dd4c14d8fe73f917e0ba092162a2a4545
r-shirani <rsh96785@gmail.com>
2023-04-22 15:21:16 +0430

    botany and chemistry lessons was added

---
+++ hagwarts/hagwarts/Botany.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hagwarts
+{
+    public class Botany
+    {
+        public List<string> Semester1Plants { get; set; }
+        public List<string> Semester2Plants { get; set; }
+        public List<string> Semester3Plants { get; set; }
+        public List<string> Semester4Plants { get; set; }
+    }
+}
