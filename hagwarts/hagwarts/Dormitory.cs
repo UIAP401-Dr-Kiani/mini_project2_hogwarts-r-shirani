7ded1fdc55bcc021097b4ee5e01cce66d9080c86
r-shirani <rsh96785@gmail.com>
2023-04-22 16:35:56 +0430

    dormitory propertu name was changed

--- hagwarts/hagwarts/Dormitory.cs
+++ hagwarts/hagwarts/Dormitory.cs
@@ -9,12 +9,12 @@ namespace hagwarts
 {
     public class Dormitory
     {
-        GroupType DormitoryGroupName { get; set; }
+        GroupType GroupType { get; set; }
         public int Floor { get; set; }
         public int Room { get; set; }
         public int Bed { get; set; }
         public string Gender { get; set; }
-        public Dormitory(string code)
+        public Dormitory(string code)//each dormitory has **6floor**10rooms**5beds
         {
             if(code.Length==3)//room number is not greater than 9
             {
@@ -28,7 +28,6 @@ namespace hagwarts
                 this.Room =10;
                 this.Floor = (int)code[3];
             }
-            
         }
     }
 }
