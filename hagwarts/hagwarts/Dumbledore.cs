5ad5fa804dce0785998710c4e39ed95ee960ee0d
r-shirani <rsh96785@gmail.com>
2023-04-22 20:52:25 +0430

    send letter function for dumbledor was complited

--- hagwarts/hagwarts/Dumbledore.cs
+++ hagwarts/hagwarts/Dumbledore.cs
@@ -11,15 +11,25 @@ namespace hagwarts
         Dormitory DormitoryList;
         public static void SendLetter(List<AllowedPerson> persons)
         {
+            int i = 1;
+            int trainNumber = 1;
+            int trainHour = 8;
             foreach (AllowedPerson p in persons)
             {
+                if(i%50==0)
+                {
+                    trainNumber++;
+                    trainHour++;
+                }
                 if(p.Role!=Role.teacher)
                 {
-                    p.ReceivedLetter = $"hello dear {p.FirstName},{p.LastName}" +
-                        $"\nwelcome to the Hagwarts";
+                    p.ReceivedLetter = $"hello dear {p.FirstName},{p.LastName}\n" +
+                        $"welcome to the Hagwarts \n" +
+                        $"your train information:\nTrain Number:{trainNumber}\nTrain Departure Time:{trainHour}" +
+                        $"remember If you don't get to the train, you have to wait an hour for the next train";
                 }
+                ++i;
             }
-
         }
     }
 }
