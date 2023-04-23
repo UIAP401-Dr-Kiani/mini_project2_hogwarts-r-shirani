6bda7cc7eb4927a9d3d86a6942718615ce009f7a
r-shirani <rsh96785@gmail.com>
2023-04-23 19:33:14 +0430

    professor menu was defined and completed

--- hagwarts/hagwarts/Menus.cs
+++ hagwarts/hagwarts/Menus.cs
@@ -30,5 +30,16 @@ namespace hagwarts
             int DumbledorChoice = Convert.ToInt32(Console.ReadLine());
             return DumbledorChoice;
         }
+        public static int ProfessorMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Professor Menu...\n");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("please enter your choice:\n");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("1.define lessons\n2.define practice\n3.set score for students\n4.confirm students grades\n5.EXIT");
+            int ProfessorChoice = Convert.ToInt32(Console.ReadLine());
+            return ProfessorChoice;
+        }
     }
 }
