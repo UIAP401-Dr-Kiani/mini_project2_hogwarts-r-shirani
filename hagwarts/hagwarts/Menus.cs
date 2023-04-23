d50f2b6940f6903b6d18f02fcb9fc11aa411dbda
r-shirani <rsh96785@gmail.com>
2023-04-23 19:15:55 +0430

    enter menu was completed

--- hagwarts/hagwarts/Menus.cs
+++ hagwarts/hagwarts/Menus.cs
@@ -8,6 +8,16 @@ namespace hagwarts
 {
     public class Menus
     {
-
+        public static int EnterMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("welcome to the Hagwarts...=)\n");
+            Console.ForegroundColor= ConsoleColor.DarkBlue;
+            Console.WriteLine("please enter your choice:\n");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("1.Dumbledor\n2.student\n3.professor\n4.EXIT");
+            int EnterChoice = Convert.ToInt32(Console.ReadLine());
+            return EnterChoice;
+        }
     }
 }
