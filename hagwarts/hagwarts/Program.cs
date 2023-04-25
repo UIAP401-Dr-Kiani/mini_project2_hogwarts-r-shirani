cc79625a1cb39158af89fec9d2cd820e16a04f43
r-shirani <rsh96785@gmail.com>
2023-04-25 22:55:53 +0430

    username and password was defined for all members

--- hagwarts/hagwarts/Program.cs
+++ hagwarts/hagwarts/Program.cs
@@ -12,32 +12,32 @@ namespace hagwarts
         static void Main(string[] args)
         {
             //****************************************************************all instances
-            Dumbledore dumbledore= new Dumbledore();
+            Dumbledore dumbledore = new Dumbledore();
             dumbledore.UserName = "ADMIN";
             dumbledore.Password = "ADMIN";
-            List<AllowedPerson> persons=new List<AllowedPerson>();
-            Plant plant=new Plant();
-            List <Lesson> lessons=new List<Lesson>();
+            List<AllowedPerson> persons = new List<AllowedPerson>();
+            Plant plant = new Plant();
+            List<Lesson> lessons = new List<Lesson>();
             Professor professor = new Professor();
             //**************************************************************for reading file
-            using (StreamReader file=new StreamReader("TXT_DATA.tsv"))
+            using (StreamReader file = new StreamReader("TXT_DATA.tsv"))
             {
                 string ln;
                 while ((ln = file.ReadLine()) != null)
                 {
-                    AllowedPerson p=new AllowedPerson();
+                    AllowedPerson p = new AllowedPerson();
                     string[] human = ln.Split('\t').ToArray<string>();
                     p.FirstName = human[0];
                     p.LastName = human[1];
-                    p.BirthDay=human[2];
+                    p.BirthDay = human[2];
                     p.Gender = human[3];
-                    p.FatherName=human[4];
-                    p.UserName=human[5];
-                    p.Password=human[6];
+                    p.FatherName = human[4];
+                    p.UserName = human[5];
+                    p.Password = human[6];
                     //----------------------------------for blood type
                     if (human[7] == "Pure blood")
                         p.BreedType = BreedType.PureBlood;
-                    else if(human[7] == "Half blood")
+                    else if (human[7] == "Half blood")
                         p.BreedType = BreedType.HalfBlood;
                     else if (human[7] == "Muggle blood")
                         p.BreedType = BreedType.MuggleBlood;
@@ -53,8 +53,8 @@ namespace hagwarts
                 file.Close();
             }
             //*****************************************************************************
-            int EnterChoice=Menus.EnterMenu();
-            while(EnterChoice!=4)
+            int EnterChoice = Menus.EnterMenu();
+            while (EnterChoice != 4)//chioce isn't EXIT
             {
                 switch (EnterChoice)
                 {
@@ -64,7 +64,7 @@ namespace hagwarts
                             string enteredUserName = Console.ReadLine();
                             Console.WriteLine("please Enter your password:");
                             string enteredPassWord = Console.ReadLine();
-                            if(enteredUserName==dumbledore.UserName && enteredPassWord==dumbledore.Password)
+                            if (enteredUserName == dumbledore.UserName && enteredPassWord == dumbledore.Password)
                             {
                                 int DumbledorChoice = Menus.DumbledorMenu();
                                 switch (DumbledorChoice)
@@ -94,16 +94,66 @@ namespace hagwarts
                                 break;
                             }
                         }
-                    case 2://professor
+                    case 2://student
                         {
+                            bool findUser = false;
                             Console.WriteLine("please Enter your username:");
-                            string enteredUserName=Console.ReadLine();
+                            string enteredUserName = Console.ReadLine();
                             Console.WriteLine("please Enter your password:");
-                            string enteredPassWord=Console.ReadLine();
-                            foreach(Person x in persons)
+                            string enteredPassWord = Console.ReadLine();
+                            foreach (var x in persons)
                             {
-                                if(x.Password==enteredPassWord && x.UserName==enteredUserName)
+                                if (x.Role == Role.student && x.Password == enteredPassWord && x.UserName == enteredUserName)
                                 {
+                                    findUser = true;
+                                    int StudentChoice = Menus.StudentMenu();
+                                    switch (StudentChoice)
+                                    {
+                                        case 1://send letter to dumbledor
+                                            {
+                                                break;
+                                            }
+                                        case 2://get information
+                                            {
+                                                break;
+                                            }
+                                        case 3://selecting unit
+                                            {
+                                                break;
+                                            }
+                                        case 4://upload homeworks
+                                            {
+                                                break;
+                                            }
+                                        case 5://exit
+                                            {
+                                                break;
+                                            }
+                                    }
+                                    break;
+                                }
+                            }
+                            if (findUser == false)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Error!\nUser with this user name and password wasn;t found\ntry again");
+                                Console.ResetColor();
+                                break;
+                            }
+                            break;
+                        }
+                    case 3://professor
+                        {
+                            bool findUser = false;
+                            Console.WriteLine("please Enter your username:");
+                            string enteredUserName = Console.ReadLine();
+                            Console.WriteLine("please Enter your password:");
+                            string enteredPassWord = Console.ReadLine();
+                            foreach (AllowedPerson x in persons)
+                            {
+                                if (x.Role == Role.teacher && x.Password == enteredPassWord && x.UserName == enteredUserName)
+                                {
+                                    findUser = true;
                                     int ProfessorChioce = Menus.ProfessorMenu();
                                     switch (ProfessorChioce)
                                     {
@@ -131,61 +181,15 @@ namespace hagwarts
                                     }
                                     break;
                                 }
-                                else
-                                {
-                                    Console.ForegroundColor= ConsoleColor.Red;
-                                    Console.WriteLine("Error!\nUser with this user name and password wasn;t found\ntry again");
-                                    Console.ResetColor();
-                                    break;
-                                }
+                            }
+                            if (findUser == false)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Error!\nUser with this user name and password wasn't found\ntry again");
+                                Console.ResetColor();
+                                break;
                             }
                             break;
-                        }
-                    case 3://student
-                        {
-                            Console.WriteLine("please Enter your username:");
-                            string enteredUserName = Console.ReadLine();
-                            Console.WriteLine("please Enter your password:");
-                            string enteredPassWord = Console.ReadLine();
-                            foreach(Person x in persons)
-                            {
-                                if(x.Password == enteredPassWord && x.UserName == enteredUserName)
-                                {
-                                    int StudentChoice = Menus.StudentMenu();
-                                    switch (StudentChoice)
-                                    {
-                                        case 1://send letter to dumbledor
-                                            {
-                                                break;
-                                            }
-                                        case 2://get information
-                                            {
-                                                break;
-                                            }
-                                        case 3://selecting unit
-                                            {
-                                                break;
-                                            }
-                                        case 4://upload homeworks
-                                            {
-                                                break;
-                                            }
-                                        case 5://exit
-                                            {
-                                                break;
-                                            }
-                                    }
-                                    break;
-                                }
-                                else
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.WriteLine("Error!\nUser with this user name and password wasn;t found\ntry again");
-                                    Console.ResetColor();
-                                    break;
-                                }
-                            }
-                            break ;
                         }
                     case 4://exit
                         break;
